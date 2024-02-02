c5b9d8584baa939f8e582e1e5ba2c32ee5c167e9
josdorrod <josediegodorado@hotmail.com>
2024-02-02 23:30:17 +0100

    Versión funcional con nº variable de cuadrados.
    Se ha añadido un botón para permitir al usuario escribir en un popup cuantos cuadrados quiere por línea y columna.

--- squaredvis.js
+++ squaredvis.js
@@ -1,29 +1,58 @@
 let container = document.querySelector(".container");
+let containerStyle = getComputedStyle(container);
 
-//Limitar altura y anchura del container a 20 veces la del cuadrado. Se le suma dos para contrarestar el borde del container.
-container.style.width = 16 * 20 + 2 + "px";
-container.style.height = 16 * 20 + 2 + "px";
 
-//Crear squares
-let numberSquares = 16*16;
-let created = 0;
 
-while(created < numberSquares){
-    let square = document.createElement("div");
-    square.className = "square";
-    container.appendChild(square);
-    created++;
+//Get container width
+let containerH = parseFloat(container.offsetWidth);
+let containerV = parseFloat(container.offsetHeight);
+let containerBorderH = parseFloat(containerStyle.borderLeftWidth) + parseFloat(containerStyle.borderRightWidth);
+let containerBorderV = parseFloat(containerStyle.borderTopWidth) + parseFloat(containerStyle.borderBottomWidth);
+let containerWidth = containerH-containerBorderH;
+let containerHeight = containerV-containerBorderV;
+
+//Create squeares
+let button = document.querySelector("button");
+button.addEventListener("click", createSquares);
+
+function createSquares(){
+
+    let numberSquares = prompt("Introduce cantidad de cuadrados por línea y columna");
+    numberSquares = numberSquares**2;
+
+    //Eliminar grid existente
+    while(container.firstChild){
+        container.removeChild(container.lastChild);
+    }
+
+    //Establecer max de 100 cuadrado por linea y columna
+    if(numberSquares >10000) alert("La cantidad introducida debe ser menor o igual a 100");
+
+    else{
+        let created = 0;
+        while(created < numberSquares){
+            let square = document.createElement("div");
+            square.style.width = (parseFloat(containerWidth)/Math.sqrt(numberSquares)) + "px";
+            square.style.height = (parseFloat(containerHeight)/Math.sqrt(numberSquares)) + "px";
+            square.className = "square";
+            container.appendChild(square);
+            created++;
+        }
+
+        //Hover Cuadrados
+        let hoverSquare = document.querySelectorAll(".square");
+        //Se convierte en array ya que el querySelectoAll devuelve un listNode.
+        Array.from(hoverSquare).forEach(element => {
+            element.addEventListener("mouseover", rellenarBackground);
+
+            function rellenarBackground(){
+                element.style.backgroundColor = "black";
+            }
+        });
+    }
+    //let numberSquares = 16*16;
 }
 
-let hoverSquare = document.querySelectorAll(".square");
 
-//Se convierte en array ya que el querySelectoAll devuelve un listNode.
-Array.from(hoverSquare).forEach(element => {
-    element.addEventListener("mouseover", rellenarBackground);
-
-    function rellenarBackground(){
-        element.style.backgroundColor = "black";
-    }
-});
 
 
